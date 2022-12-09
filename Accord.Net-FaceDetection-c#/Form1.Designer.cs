5c162fceea638c597850d43a68cdf6ea24080113
Muhammad Nuhsyah putra <putramirza712@gmail.com>
2022-12-09 18:41:13 +0800

    Do FaceDetectioninC#usingaccord.net

---
+++ Accord.Net-FaceDetection-c#/Form1.Designer.cs
@@ -0,0 +1,123 @@
+﻿namespace winformapp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button2 = new System.Windows.Forms.Button();
+            this.cmbcamera = new System.Windows.Forms.ComboBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.videoSourcePlayer1 = new Accord.Controls.VideoSourcePlayer();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(873, 336);
+            this.button2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(267, 28);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Use Camera Device";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // cmbcamera
+            // 
+            this.cmbcamera.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cmbcamera.FormattingEnabled = true;
+            this.cmbcamera.Location = new System.Drawing.Point(873, 257);
+            this.cmbcamera.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.cmbcamera.Name = "cmbcamera";
+            this.cmbcamera.Size = new System.Drawing.Size(265, 24);
+            this.cmbcamera.TabIndex = 17;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.pictureBox2.Location = new System.Drawing.Point(873, 4);
+            this.pictureBox2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(265, 245);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox2.TabIndex = 20;
+            this.pictureBox2.TabStop = false;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(873, 417);
+            this.button1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(267, 28);
+            this.button1.TabIndex = 21;
+            this.button1.Text = "Camera Properties";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // videoSourcePlayer1
+            // 
+            this.videoSourcePlayer1.Location = new System.Drawing.Point(4, 4);
+            this.videoSourcePlayer1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.videoSourcePlayer1.Name = "videoSourcePlayer1";
+            this.videoSourcePlayer1.Size = new System.Drawing.Size(853, 492);
+            this.videoSourcePlayer1.TabIndex = 22;
+            this.videoSourcePlayer1.Text = "videoSourcePlayer1";
+            this.videoSourcePlayer1.VideoSource = null;
+            this.videoSourcePlayer1.NewFrame += new Accord.Controls.VideoSourcePlayer.NewFrameHandler(this.videoSourcePlayer1_NewFrame);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.GrayText;
+            this.ClientSize = new System.Drawing.Size(1153, 502);
+            this.Controls.Add(this.videoSourcePlayer1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.cmbcamera);
+            this.Controls.Add(this.button2);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Accord.Net Web Camera Face Detection C# by:  Putra";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.ComboBox cmbcamera;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Button button1;
+        private Accord.Controls.VideoSourcePlayer videoSourcePlayer1;
+    }
+}
+
---
+++ Accord.Net-FaceDetection-c#/Form1.cs
@@ -0,0 +1,197 @@
+﻿using System;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Windows.Forms;
+using System.Threading;
+using System.IO;
+
+using Accord;
+using Accord.Imaging.Filters;
+using Accord.Video;
+using Accord.Video.DirectShow;
+using Accord.Vision.Detection;
+using Accord.Vision.Detection.Cascades;
+
+
+namespace winformapp
+{
+
+    public partial class Form1 : Form
+    {
+
+        RectanglesMarker marker;
+        ResizeNearestNeighbor res = new ResizeNearestNeighbor(200, 200);
+        ResizeNearestNeighbor resize = new ResizeNearestNeighbor(320, 200);
+        Rectangle[] faces = null;
+        internal string aliasname = "Unknown";
+
+        FilterInfoCollection videoDevices;
+        private string ldevice;
+        private IVideoSource videoSource = null;
+        HaarObjectDetector detector;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            detector = new HaarObjectDetector(new FaceHaarCascade(), 32, ObjectDetectorSearchMode.Average, 1.5f, ObjectDetectorScalingMode.GreaterToSmaller);
+            detector.MaxSize = new Size(320, 320);
+            detector.UseParallelProcessing = true;
+            detector.Suppression = 2;
+
+            listlocalcamera();
+        }
+
+        public static void SetDoubleBuffered(System.Windows.Forms.Control c)
+        {
+            System.Reflection.PropertyInfo aProp =
+                  typeof(System.Windows.Forms.Control).GetProperty(
+                        "DoubleBuffered",
+                        System.Reflection.BindingFlags.NonPublic |
+                        System.Reflection.BindingFlags.Instance);
+            aProp.SetValue(c, true, null);
+        }
+
+        private void listlocalcamera()
+        {
+            try
+            {
+                videoDevices = new FilterInfoCollection(FilterCategory.VideoInputDevice);
+                if (videoDevices.Count == 0) return;
+                for (int i = 0; i < videoDevices.Count; i++)
+                {
+                    cmbcamera.Items.Add(videoDevices[i].Name);
+                }
+                cmbcamera.SelectedIndex = 0;
+            }
+            catch
+            {
+            }
+        }
+
+        private void uselocalcamera(int camnum)
+        {
+            try
+            {
+                videoDevices = new FilterInfoCollection(FilterCategory.VideoInputDevice);
+                if (videoDevices.Count == 0) return;
+                ldevice = videoDevices[camnum].MonikerString;
+                selectlocalvideodevice(ldevice);
+            }
+            catch
+            {
+            }
+        }
+
+        private void selectlocalvideodevice(string dev)
+        {
+            VideoCaptureDevice videoSource = new VideoCaptureDevice(dev);
+            videoSource.VideoResolution = selectResolution(videoSource);
+            OpenVideoSource(videoSource);
+        }
+
+        private static VideoCapabilities selectResolution(VideoCaptureDevice device)
+        {
+            foreach (var cap in device.VideoCapabilities)
+            {
+                if (cap.FrameSize.Width == 1280)
+                    return cap;
+                if (cap.FrameSize.Height == 800)
+                    return cap;
+            }
+            return device.VideoCapabilities.Last();
+        }
+
+        private void OpenVideoSource(IVideoSource source)
+        {
+            CloseVideoSource();
+            videoSourcePlayer1.VideoSource = source;
+            videoSource = source;
+            videoSourcePlayer1.Start();
+        }
+
+        private void CloseVideoSource()
+        {
+            videoSourcePlayer1.SignalToStop();
+            while (videoSourcePlayer1.IsRunning)
+            {
+                Thread.Sleep(100);
+            }
+
+                videoSourcePlayer1.Stop();
+        }
+
+        private void writetoframeimage(string aliasname, Bitmap image, int linex, int liney)
+        {
+            Graphics g = Graphics.FromImage(image);
+            g.DrawString(aliasname, this.Font, Brushes.CornflowerBlue, new PointF(linex, liney));
+            g.Flush();
+        }
+
+
+        private void videoSourcePlayer1_NewFrame(object sender, ref Bitmap image)
+        {
+            Bitmap ximage = image;
+            ximage = resize.Apply(ximage);
+            faces = detector.ProcessFrame(ximage);
+            if (faces.Length > 0)
+            {
+                for (var i = 0; i < faces.Length; i++)
+                {
+                    faces[i].X -= 10;
+                    faces[i].Y -= 10;
+                    faces[i].Height += 20;
+                    faces[i].Width += 20;
+                    var faceimage = new Crop(faces[i]).Apply(ximage);
+                    faceimage = res.Apply(faceimage);
+                    pictureBox2.Image = faceimage;
+                    writetoframeimage("Unknown", ximage, faces[i].X - 4, faces[i].Y - 14);
+                }
+            }
+            marker = new RectanglesMarker(faces);
+            marker.MarkerColor = Color.DeepSkyBlue;
+            marker.ApplyInPlace(ximage);
+            image = ximage;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (cmbcamera.Items.Count > 0)
+            {
+                videoSourcePlayer1.BringToFront();
+                uselocalcamera(cmbcamera.SelectedIndex);
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            SetDoubleBuffered(videoSourcePlayer1);
+            SetDoubleBuffered(pictureBox2);
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            videoSourcePlayer1.Dispose();
+            Environment.Exit(0);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if ((videoSource != null) && (videoSource is VideoCaptureDevice))
+            {
+                try
+                {
+                    ((VideoCaptureDevice)videoSource).DisplayPropertyPage(this.Handle);
+                }
+                catch (NotSupportedException)
+                {
+                    MessageBox.Show("The video source does not support configuration property page.", "Error",
+                    MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+
+        }
+
+    }
+}
